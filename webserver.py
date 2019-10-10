b6d6fe7b979d1a037af5643ed0cdbef8ce8495ad
welschmichel <47657446+welschmichel@users.noreply.github.com>
2019-10-10 15:02:58 +0200

    Datum neu dd.mm.yyyy

--- webserver.py
+++ webserver.py
@@ -20,7 +20,14 @@ def input():
 		vorname=request.form['vorname']
 		date=request.form['date']
 		vorname = vorname.capitalize()
-		return render_template("input.html", vorname=vorname, date=date)
+
+
+		single_date = date.split("-")
+		year = single_date[0]
+		month = single_date[1]
+		day = single_date[2] + "."
+
+		return render_template("input.html", vorname=vorname, date=date, single_date=single_date, day=day, month=month, year=year)
 	else:
 		return render_template("input.html")
 
@@ -28,3 +35,6 @@ def input():
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
 
+
+    def date_rechner():
+    	monate = {1:"januar", 2:"februar"}
