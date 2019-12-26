e3cfeec908efdee9ab5ffc27b20b79cc394288c8
welschmichel <47657446+welschmichel@users.noreply.github.com>
2019-12-26 16:04:59 +0100

    added json, jsonify

--- webserver.py
+++ webserver.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, jsonify,json
 
 app = Flask(__name__)
 
@@ -13,6 +13,38 @@ def welcome():
 #	if request.method=="POST":
 #		vorname = request.form['vorname']
 #	return render_template("input.html", vorname=vorname)
+
+# Test-Route for JSON
+@app.route('/json')
+def json():
+	try:
+		meine_liste = []
+		for i in range(0,3):
+			mein_dict = {
+			'vorname':'Michel',
+			'nachname': 'Welsch'}
+			meine_liste.append(mein_dict)
+
+		jsonStr = json.dumps(meine_liste)
+
+	except:
+		print('fehler')
+
+	return render_template("json.html", meine_liste=meine_liste)
+	return jsonify(meine_liste)
+
+
+
+
+
+
+
+
+	#notiz1 = 'Hallo ich bin der Text aus der Notiz'
+	#datum1 = '19. Januar 2020'
+	#notiz2 = 'ich bin die zweite Notiz'
+	#datum2 = '01.01.2022'
+	#return render_template('json.html')
 
 
 @app.route('/', methods=["POST", "GET"])
